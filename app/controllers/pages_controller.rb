7787580c4b97e93ec07d9d8f174fd495b32138f7
Hossam-Allam <h.refaie@stu.khas.edu.tr>
2025-02-07 16:13:10 +0300

    using flickr gem to search for photos of a given user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def index
+    if params[:user_id].present?
+      flickr = Flickr.new(
+        api_key: ENV["FLICKR_API_KEY"],
+        shared_secret: ENV["FLICKR_SHARED_SECRET"]
+      )
+
+      @photos = flickr.photos.search(user_id: params[:user_id])
+    end
+  rescue => e
+    flash.now[:error] = "Error fetching photos: #{e.message}"
+    @photos = []
   end
 end
